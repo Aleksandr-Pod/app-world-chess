9c22a8a542a39edeb943e49d2be13da3a5f372b4
slabak <slabakxaker@gmail.com>
2022-08-19 20:41:23 +0300

    base board1

--- src/components/gameBoard/GameBoard.tsx
+++ src/components/gameBoard/GameBoard.tsx
@@ -18,6 +18,7 @@ const GameBoard = () => {
         };
         createSquare();
         console.log(board);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
     return (
         <BoxGame>
