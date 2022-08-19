593ef5bbdd6657ae926d285e84088ea44c451977
sla6ak <86052868+sla6ak@users.noreply.github.com>
2022-08-19 17:43:22 +0000

    Deploying to gh-pages from @ sla6ak/app-world-chess@9c22a8a542a39edeb943e49d2be13da3a5f372b4 🚀

--- static/js/151.caec4ddf.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkapp_world_chess=self.webpackChunkapp_world_chess||[]).push([[151],{3151:function(e,s,c){c.r(s);var n=c(2791),r=c(6871),u=c(184);s.default=function(){var e=(0,r.s0)();return(0,n.useEffect)((function(){e("/home")}),[]),(0,u.jsx)("div",{children:"dashboard"})}}}]);
-//# sourceMappingURL=151.caec4ddf.chunk.js.map
