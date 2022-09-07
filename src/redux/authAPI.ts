1aea39d33ec832c2ba7a0ce2a98b485b7139a0f9
slabak <slabakxaker@gmail.com>
2022-09-07 06:26:27 +0300

    ws linck

--- src/redux/authAPI.ts
+++ src/redux/authAPI.ts
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import BASE_URL from "./testURL";
+import { BASE_URL } from "./testURL";
 
 export const authApi = createApi({
     reducerPath: "userApi",
